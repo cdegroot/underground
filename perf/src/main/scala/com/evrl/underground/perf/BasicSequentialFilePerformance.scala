a6bcb434df683816c9a60bdb3b6af586b46c9e42
Cees de Groot <cdegroot@ebay.com>
2011-11-04 13:07:35 +0100

    Tuned iterations a bit to minimize task executions (and thus persistence setup/teardown) per benchmark run

--- perf/src/main/scala/com/evrl/underground/perf/BasicSequentialFilePerformance.scala
+++ perf/src/main/scala/com/evrl/underground/perf/BasicSequentialFilePerformance.scala
@@ -8,16 +8,17 @@ import com.evrl.underground.{IncomingMessage, BasicSequentialFilePersistence}
  * glue to be expected :)
  */
 object BasicSequentialFilePerformance extends App {
+  val iterations = 100000
   val task = new Runnable {
     def run {
       val persistence = BasicSequentialFilePersistence.onRandomDirectory
-      val message = new IncomingMessage("hello, world".getBytes)
-      for (i <- 0 until 10) {
+      val message = new IncomingMessage("hello, world, how are you".getBytes) // 25 bytes if you want to calculate throughput
+      for (i <- 0 until iterations) {
         persistence.persist(message)
       }
       persistence.destroyData
     }
   }
 
-  System.out.println("BasicSequentialFilePerformance: " + new Benchmark(task))
+  System.out.println("BasicSequentialFilePerformance: \n" + new Benchmark(task, iterations).toStringFull)
 }
